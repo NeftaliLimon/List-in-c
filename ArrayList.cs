02609341d818e87f47bd8b2dcbd6e78a23e2f1cf
NeftaliLimon <nef.nl66@gmail.com>
2022-03-19 23:02:27 -0600

    Agregar archivos de proyecto.

---
+++ ArrayList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class ArrayList<T>:List<T>
+    {
+        private const int DEFAULT_SIZE = 2;
+        private T[] array;
+        private int size;
+
+        public ArrayList(int size)
+        {
+            array = (T[])new T[size];
+        }
+
+        public ArrayList()
+        {
+            array = (T[])new T[DEFAULT_SIZE];
+        }
+
+        
+    public void addAtTail(T data)
+        {
+
+            if (size == array.Length)
+            {
+                increaseArraySize();
+            }
+            array[size] = data;
+            size++;
+        }
+        
+    public void addAtFront(T data)
+        {
+
+            if (size == array.Length)
+            {
+                increaseArraySize();
+            }
+            if (size >= 0)
+            {
+                Array.Copy(array, 0, array, 1, size);
+            }
+            array[0] = data;
+            size++;
+        }
+        
+    public void remove(int index)
+        {
+
+            if (index < 0 || index >= size)
+            {
+                return;
+            }
+            if (size - 1 - index >= 0)
+            {
+                Array.Copy(array, index + 1, array, index, size - 1 - index);
+            }
+            array[size - 1] = default;
+            size--;
+        }
+     
+    public void removeAll()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = default;
+            }
+            size = 0;
+        }
+        
+    public T getAt(int index)
+        {
+            if(index>=0 && index < size)
+                return array[index];
+            else
+                return default;
+        }
+        
+    public void setAt(int index, T data)
+        {
+            if (index >= 0 && index < size)
+            {
+                array[index] = data;
+            }
+        }
+
+       
+    public Iterator<T> getIterator()
+        {
+            return new ArrayListIterator<T>(this);
+        }
+
+     
+    public int getSize()
+        {
+            return size;
+        }
+
+        private void increaseArraySize()
+        {
+            T[] newArray = (T[])new T[array.Length * 2];
+
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+    }
+}
---
+++ LinkedList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class LinkedList<T>:List<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private int size;
+
+        
+    public void addAtTail(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (size == 0)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.next = node;
+                node.previous = tail;
+            }
+
+            tail = node;
+            size++;
+        }
+
+        
+    public void addAtFront(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (size == 0)
+            {
+                tail = node;
+            }
+            else
+            {
+                head.previous = node;
+            }
+            node.next = head;
+            head = node;
+
+            size++;
+        }
+
+        
+    public void remove(int index)
+        {
+            Node<T> node = findNode(index);
+
+            if (node == null)
+            {
+                return;
+            }
+
+            if (size == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else if (node == head)
+            {
+                head = node.next;
+                if (head != null)
+                {
+                    head.previous = null;
+                }
+            }
+            else if (node == tail)
+            {
+                tail = node.previous;
+                if (tail != null)
+                {
+                    tail.next = null;
+                }
+            }
+            else
+            {
+                node.previous.next = node.next;
+                node.next.previous = node.previous;
+            }
+            size--;
+        }
+
+        
+    public void removeAll()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        
+    public void setAt(int index, T data)
+        {
+            Node<T> node = findNode(index);
+
+            if (node != null)
+            {
+                node.data = data;
+            }
+        }
+
+        
+    public T getAt(int index)
+        {
+            Node<T> node = findNode(index);
+
+            if (node == null)
+                return default;
+            else
+                return node.data;
+        }
+
+    public Iterator<T> getIterator()
+        {
+            return new LinkedListIterator<T>(head);
+        }
+
+   
+    public int getSize()
+        {
+            return size;
+        }
+
+        private Node<T> findNode(int index)
+        {
+
+            if (index < 0 || index >= size)
+            {
+                return null;
+            }
+
+            Node<T> node = head;
+            int currentIndex = 0;
+
+            while (currentIndex != index)
+            {
+                currentIndex++;
+                node = node.next;
+            }
+
+            return node;
+        }
+    }
+}
